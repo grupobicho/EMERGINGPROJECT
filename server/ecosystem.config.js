da85a9630770d8db74f2e29c2f641c0a6e3db32b
Wagner Da gloria <wagnerdagloria@Wagners-MacBook-Pro.local>
2024-12-25 19:54:19 +0000

    initial commit

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    apps:[
-        {
-            name: "EMERGINGPROJECT",
-            script: "npm ",
-            args: "run dev",
-            instances:1,
-            autorestart:true,
-            watch:false,
-            env: {
-                NODE_ENV: "development",
-                ENV_VAR1: "enviroment-variable",
-            },
-
-        },
-
-    ], 
-};
+    apps: [{
+      name: "EMERGINGPROJECT",
+      script: "npm",
+      args: "run dev",  // replace with your main file path
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }]
+  }
